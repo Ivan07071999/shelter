8d9b56265caeee7a44830b793ed8fe56d6abe320
Ivan07071999 <ivan99mikh@gmail.com>
2025-01-30 01:52:44 +0300

    burger togle

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,39 @@
 const openPopUp = document.querySelector('.butt_pets2');
 const closePopUpButton = document.querySelector('.close');
 const popUpJennifer = document.querySelector('.popUpJennifer');
+const hamburgerMenu = document.querySelector('.menu__btn');
+const navigation = document.querySelector('.header_navigation')
+
+const scrollController = {
+   disablesScroll() {
+      document.body.style.cssText = `
+      overflow: hidden;
+      `
+   },
+   enabledScroll() {
+      document.body.style.cssText = ''
+   },
+}
+
+
+
 
 function handlerOpenPopup() {
     popUpJennifer.classList.remove("hidden")
+    scrollController.disablesScroll()
 }
 
 openPopUp.addEventListener('click', handlerOpenPopup)
 
 popUpJennifer.addEventListener('click', (event) => {
     if(event.target.classList.contains('popUpJennifer')) {
+      scrollController.enabledScroll()
         popUpJennifer.classList.toggle('hidden')
     }
  })
 
  closePopUpButton.addEventListener('click', () => {
+   scrollController.enabledScroll()
     popUpJennifer.classList.toggle('hidden')
  })
 
@@ -26,11 +45,13 @@ popUpJennifer.addEventListener('click', (event) => {
 
  openPopUpButtonKat.addEventListener('click', () => {
     popUpKatrine.classList.toggle('hiddenKatrine')
+    scrollController.disablesScroll()
  })
 
  popUpKatrine.addEventListener('click', (event) => {
+   scrollController.enabledScroll()
     if(event.target.classList.contains('popUpKatrine')) {
-        popUpKatrine.classList.toggle('hiddenKatrine')
+      popUpKatrine.classList.toggle('hiddenKatrine')  
     }
  })
 
@@ -44,14 +65,17 @@ popUpJennifer.addEventListener('click', (event) => {
 
  openPopUpButtonWoody.addEventListener('click', () => {
     popUpWoody.classList.toggle('hiddenWoody')
+    scrollController.disablesScroll()
  })
 
  popUpWoody.addEventListener('click', (event) => {
     if(event.target.classList.contains('popUpWoody')) {
+      scrollController.enabledScroll()
         popUpWoody.classList.toggle('hiddenWoody')
     }
  })
 
  closePopUpButtonWoody.addEventListener('click', () => {
     popUpWoody.classList.toggle('hiddenWoody')
+    scrollController.enabledScroll()
  })
